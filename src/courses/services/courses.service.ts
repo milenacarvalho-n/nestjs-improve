fea86457bdeaf1ea2f3d30142c12e59e8c361045
milenacarvalho-n <nascarvalho.m@gmail.com>
2023-11-22 16:42:38 -0300

    create entity

--- src/courses/services/courses.service.ts
+++ src/courses/services/courses.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from '../entities/courses.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "NodeJS",
+            description: 'Curso básico sobre nodejs',
+            tags: ['basic','nodejs', 'javascript'],
+
+        }
+    ]
+
+    findAll(){
+       return this.courses
+    }
+}
