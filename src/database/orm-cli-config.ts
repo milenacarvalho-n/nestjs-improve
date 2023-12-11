00e31814be78009166394904fc604c414573965e
milenacarvalho <nascarvalho.m@gmail.com>
2023-12-11 17:44:24 -0300

    feat: migrations

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1702302124811 } from "src/migrations/1702302124811-CreateCoursesTable";
+import { CreateTagsTable1702320841305 } from "src/migrations/1702320841305-CreateTagsTable";
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCoursesTable1702302124811],
+    migrations: [CreateCoursesTable1702302124811, CreateTagsTable1702320841305],
 })
